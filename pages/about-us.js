c5d53bbede0209576eb2256b067d70f54bebedc8
Hassan-Ib <azanebrahim@gmail.com>
2022-05-02 07:08:36 -0600

    about person card done

--- pages/about-us.js
+++ pages/about-us.js
@@ -2,56 +2,65 @@ import React from "react";
 import { getLayoutWithNavAndFooter, Link } from "../components";
 import Image from "next/image";
 import Svg from "../public/undraw_exams_g-4-ow.svg";
-import {AiOutlineDownload} from "react-icons/ai"
+import { AiOutlineDownload } from "react-icons/ai";
 
-const ImportantFigure = ({ title, name }) => {
+function ImportantFigure(props) {
   return (
-    <article className="relative w-[80%] flex flex-col gap-4 max-w-sm md:max-w-2xl md:flex-row rounded-md bg-white shadow-md p-4">
-      <figure className="relative md:w-[60%] block group overflow-hidden rounded">
-        <Image
-          className="group-hover:transform group-hover:scale-110 transition-transform duration-300"
-          src={"/img/leon-wu-LLfRMRT-9AY-unsplash.jpg"}
-          alt="cto"
-          width="500"
-          height="400"
-        />
-        <figcaption className="absolute bottom-4 left-0 ">
-          <h3 className="text-xl font-medium text-white bg-gradient-to-r from-birch-500 via-birch-500 to-twine-300 max-w-min">      
-            Ibrahim
-            Hassanayomide
+    <article className="rounded-md bg-white shadow-md  p-2 sm:p-4 w-[90%] max-w-sm md:max-w-2xl">
+      <figure className="relative w-full h-96 group flex flex-row gap-4">
+        <div className="relative w-full h-full">
+          <Image
+            className={
+              "group-hover:transform group-hover:scale-110 transition-transform duration-300"
+            }
+            alt="cto"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="top"
+            src={`/about/${props.url}`}
+          />
+        </div>
+        <figcaption className="absolute bg-black/40 text-white md:text-black z-10 bottom-0 left-0 md:static px-2 py-6">
+          <h3 className="mb-2 font-medium text-birch/90 max-w-min uppercase tracking-widest">
+            Dr. Ibrahim Hassanayomide
           </h3>
+          <p className="  opacity-80 md:py-6">
+            lorem ipsum dolor sit amet consectetur adipisicing elit. laboriosam,
+            laboriosam justify
+          </p>
+          <div className="flex justify-end"></div>
         </figcaption>
       </figure>
-      <section className="tracking-widest  bg-slate-800">
-        {title}
-      </section>
     </article>
   );
-};
+}
 
 const About = () => {
   return (
     <main className="">
-      <section className="flex flex-col lg:flex-row gap-14 place-items-center px-6 py-10 pb-10 mb-20">
-        <div  className="flex flex-col gap-10 items-center justify-center md:items-start max-w-2xl ">
-          <h2 className="text-4xl text-center lg:text-left font-bold">
+      <section className="flex flex-col lg:flex-row lg:h-[80vh] gap-14 place-items-center px-6 pt-16 pb-10 mb-20">
+        <div className="flex flex-col gap-10 items-center justify-center md:items-start max-w-2xl ">
+          <h2 className="text-4xl sm:text-5xl sm:leading-[1.2] text-center lg:text-left font-bold">
             Complete your dream in computing, security and prgramming.
           </h2>
-          <p className="leading-5 md:leading-6 opacity-80 text-center lg:text-left">
-            Learn any programing language, and build your own future,
-            you can learn from us onsite or online with us.
-            we are open to application anytime, and if you want a project done we have certified, capable student to get it done, click on contact below
+          <p className=" leading-5 md:leading-6 opacity-80 text-center lg:text-left">
+            Learn any programing language, and build your own future, you can
+            learn from us onsite or online with us. we are open to application
+            anytime, and if you want a project done we have certified, capable
+            student to get it done, click on contact below
           </p>
-          <div className="flex flex-col text-center gap-4 self-stretch border-2 border-red-500">
-
+          <div className="flex flex-col sm:flex-row justify-center lg:justify-start text-center gap-4 self-stretch">
             <button className="btn btn-md btn-block flex justify-center gap-3 items-center">
-              <AiOutlineDownload className="text-xl"/> Download our guide 
+              <AiOutlineDownload className="text-xl" /> Download our guide
             </button>
-            <Link href="/contacts" className="btn btn-md btn-outline-black" > Contact us </Link>
+            <Link href="/contacts" className="btn btn-md btn-outline-black">
+              {" "}
+              Contact us{" "}
+            </Link>
           </div>
         </div>
         <div className="flex items-center">
-          <Image src={Svg}  alt="student sitting for exam svg" />
+          <Image src={Svg} alt="student sitting for exam svg" />
         </div>
       </section>
       <section className="md:px-10 py-10 my-20 ">
@@ -65,9 +74,18 @@ const About = () => {
         </div>
         {/* gallary for the importants */}
         <div className="flex flex-col gap-16 items-center py-16">
-          <ImportantFigure title={"VC"} />
-          <ImportantFigure title={"Hod"} />
-          <ImportantFigure title={"Hod"} />
+          <ImportantFigure
+            title={"VC"}
+            url={"jurica-koletic-7YVZYZeITc8-unsplash.jpg"}
+          />
+          <ImportantFigure
+            title={"Hod"}
+            url={"linkedin-sales-solutions-pAtA8xe_iVM-unsplash.jpg"}
+          />
+          <ImportantFigure
+            title={"Hod"}
+            url={"olga-zabegina-A3MleA0jtoE-unsplash.jpg"}
+          />
         </div>
       </section>
       <section className="md:px-10 my-10">
